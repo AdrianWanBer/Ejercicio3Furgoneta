dac7d5afd6483e1d3e6ae09da4c3bc355638437c
DESKTOP-3T85H9P\Adrian <csi23-awanuer@colegioaltair.net>
2024-01-16 13:33:15 +0100

    Agregar archivos de proyecto.

---
+++ Ejercicio3Furgoneta/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Ejercicio3Furgoneta
+{
+    class program
+    {
+        public static void Main (string[] args)
+        {
+            Console.WriteLine("Cuanto pesa tu saco");
+            int pesoSaco = Convert.ToInt32 (Console.ReadLine());
+
+            int numeroSacosMax = 4000 / pesoSaco;
+            Console.WriteLine(numeroSacosMax);
+        }
+    }
+}
